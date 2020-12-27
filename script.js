f010af4eae5ca0276c0828c3545fde00462986a3
MDjavaheri <michael.djavaheri@mail.yu.edu>
2020-12-26 20:18:25 -0500

    fix for all files in dir, outputs to files

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
-const sefer = require('./vol1.json');
-cosnt fs = require('fs');
-const seferMap = new Map(Object.entries(sefer.text));
+const fs = require('fs');
+let filenames = fs.readdirSync("./books"); 
 let toc = [];
 
 function subheading(line) {
     return line.match(/<b>/); 
 }
-function mapper(o) {
+function mapper(o, fileName) {
     let output = [];
+    let outText = `${fileName}\n`;
     o.forEach((entries, key) => {
         if (Array.isArray(entries)) {
             let headings = entries.filter(subheading)
                                   .map(line => line.trim().substring(line.lastIndexOf('<b>')+3, line.length - 4));
             output[key] = headings;
+            outText += `${key}: \n`;
+            headings.forEach(h => {
+                outText += `-${h} \n`;
+            });
         }
         else {
             let sub = mapper(new Map(Object.entries(entries)));
-            output[key] = sub;
+            output[key] = sub[0];
+            outText += `${key}: \n`;
+            outText += `${sub[1]} \n`;
         }
     });
-    return output;
+    return [output, outText];
 }
-toc = mapper(seferMap);
-
-
+  
+filenames.forEach((file) => { 
+    let sefer = require(`./books/${file}`);
+    let vol = file.substring(0, 4);
+    const seferMap = new Map(Object.entries(sefer.text));
+    toc = mapper(seferMap, vol);
+    console.log(toc);
+    fs.writeFile(`${vol}-toc.txt`, toc, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    })});
